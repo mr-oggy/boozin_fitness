2a997c191cfdc6c304f0faeb7c03a31754f9dbb6
mr-oggy <abhishekkumargupta11@gmail.com>
2022-01-09 17:26:21 +0530

    feat: shimmer

---
+++ lib/src/utils/loading_shimmer.dart
@@ -0,0 +1,56 @@
+import 'package:boozin_fitness/src/utils/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:shimmer_animation/shimmer_animation.dart';
+
+class LoadingShimmer extends StatelessWidget {
+  const LoadingShimmer({
+    required this.loading,
+    required this.child,
+    this.width,
+    this.height,
+    this.borderRadius,
+    Key? key,
+  }) : super(key: key);
+
+  final Widget? child;
+  final double? width;
+  final double? height;
+  final bool loading;
+
+  final BorderRadius? borderRadius;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSwitcher(
+      duration: const Duration(milliseconds: 500),
+      transitionBuilder: _transitionBuilder,
+      child: loading
+          ? ClipRRect(
+              borderRadius: borderRadius ?? BorderRadius.circular(10),
+              child: Shimmer(
+                duration: const Duration(seconds: 2),
+                color: AppColor.black.withOpacity(0.005),
+                child: Container(
+                  color: AppColor.black.withOpacity(0.05),
+                  alignment: Alignment.center,
+                  child: ConstrainedBox(
+                    constraints: BoxConstraints(
+                      minHeight: height ?? 0,
+                      minWidth: width ?? 0,
+                    ),
+                    child: IgnorePointer(
+                      child: Opacity(
+                        opacity: 0,
+                        child: child,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            )
+          : child,
+    );
+  }
+
+  Widget _transitionBuilder(child, animation) => FadeTransition(opacity: animation, child: child);
+}
