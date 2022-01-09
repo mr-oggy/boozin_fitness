5391ee22098e82461ab275a639418b3490aebe84
mr-oggy <abhishekkumargupta11@gmail.com>
2022-01-09 17:26:33 +0530

    fix: added strings

--- lib/src/utils/strings.dart
+++ lib/src/utils/strings.dart
@@ -1,4 +1,7 @@
 class AppText {
   static const String fitness = "Fitness";
   static const String hi = "Hi!";
+  static const String steps = "Steps";
+  static const String caloriesBurned = "Calories Burned";
+  static const String goal = "Goal";
 }
