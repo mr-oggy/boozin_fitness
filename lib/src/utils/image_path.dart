9b7ddafb900a9eff119b40ece55237bbd1eb433d
mr-oggy <abhishekkumargupta11@gmail.com>
2022-01-09 13:50:18 +0530

    feat: app images or icons

---
+++ lib/src/utils/image_path.dart
@@ -0,0 +1,16 @@
+class ImagePathDark {
+  static const String splashBoozin = "assets/splash_logo/dark/Booz_n.png";
+  static const String iconFootSteps = "assets/icons/dark/footsteps.png";
+  static const String iconKcal = "assets/icons/dark/kcal.png";
+}
+
+class ImagePathLight {
+  static const String splashBoozin = "assets/splash_logo/light/Booz_n.png";
+  static const String iconFootSteps = "assets/icons/light/footsteps.png";
+  static const String iconKcal = "assets/icons/light/kcal.png";
+}
+
+class ImagePathCommon {
+  static const String splashI = "assets/splash_logo/i_common.png";
+  static const String splashOO = "assets/splash_logo/oo_common.png";
+}
